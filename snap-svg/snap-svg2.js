a2c80c14ce94de8876b7958378e98abd2dc9e1a3
scheakur <scheakur@gmail.com>
2014-06-15 09:25:28 +0900

    Refactoring: Rename variables

--- snap-svg/snap-svg2.js
+++ snap-svg/snap-svg2.js
@@ -46,9 +46,9 @@
     return (el) ? el.getBBox().y2 : 0;
   }
 
-  function addTextBox(text, x, y, from) {
+  function addTextBox(text, x, y, start) {
     x = x || center;
-    y = y || bottom(from || last);
+    y = y || bottom(start || last);
     var margin = 120;
     var t = paper.text(x, y + margin, text);
     t.attr({
@@ -67,7 +67,7 @@
 
     var g = paper.group(r, t);
 
-    addConnectorArrow(from || last, g);
+    addConnectorArrow(start || last, g);
 
     last = g;
 
@@ -80,9 +80,9 @@
     var y = bottom(last);
     var ux = width / (texts.length + 1);
     var x = ux;
-    var from = last;
+    var start = last;
     texts.forEach(function(text) {
-      addTextBox(text, x, y, from);
+      addTextBox(text, x, y, start);
       x += ux;
     });
   }
@@ -98,45 +98,45 @@
   var tipH = 16;
   var tipW = 10;
 
-  function addConnectorArrow(from, to) {
-    if (!from || !to) {
+  function addConnectorArrow(start, end) {
+    if (!start || !end) {
       return;
     }
 
-    var bf = from.getBBox();
-    var xf = bf.cx;
-    var yf = bf.y2;
+    var bs = start.getBBox();
+    var xs = bs.cx;
+    var ys = bs.y2;
 
-    var bt = to.getBBox();
-    var xt = bt.cx;
-    var yt = bt.y;
+    var be = end.getBBox();
+    var xe = be.cx;
+    var ye = be.y;
 
-    if (Math.abs(xt - xf) < 10 || Math.abs(yt - yf) < 10) {
-      straight(xf, yf, xt, yt);
+    if (Math.abs(xe - xs) < 10 || Math.abs(ye - ys) < 10) {
+      straight(xs, ys, xe, ye);
     } else {
-      curve(xf, yf, xt, yt);
+      curve(xs, ys, xe, ye);
     }
   }
 
-  function straight(xf, yf, xt, yt) {
-    var bar = paper.path(fmt('M %s %s L %s %s', xf, yf, xt, yt)).attr({
+  function straight(xs, ys, xe, ye) {
+    var bar = paper.path(fmt('M %s %s L %s %s', xs, ys, xe, ye)).attr({
       class: 'arrow'
     });
-    paper.group(bar, tip(xt, yt));
+    paper.group(bar, tip(xe, ye));
   }
 
-  function curve(xf, yf, xt, yt) {
-    var ydiff = yf - yt;
+  function curve(xs, ys, xe, ye) {
+    var ydiff = ys - ye;
     var invert = ydiff > 0;
     var bar = paper.path(fmt('M %s %s Q %s %s %s %s %s %s %s %s',
-        xf, yf,
-        xf, invert ? yf + ydiff : yt,
-        xf + (xt - xf) / 2, yf + (yt - yf) / 2,
-        xt, invert ? yt - ydiff : yf,
-        xt, yt)).attr({
+        xs, ys,
+        xs, invert ? ys + ydiff : ye,
+        xs + (xe - xs) / 2, ys + (ye - ys) / 2,
+        xe, invert ? ye - ydiff : ys,
+        xe, ye)).attr({
       class: 'arrow'
     });
-    paper.group(bar, tip(xt, yt));
+    paper.group(bar, tip(xe, ye));
   }
 
   function tip(x, y) {
