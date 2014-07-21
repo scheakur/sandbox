a44c059faae64679253e08d5a5ead8b754de3884
scheakur <scheakur@gmail.com>
2014-07-21 12:07:13 +0900

    Rename package

--- springboot-example/src/main/java/com/scheakur/sandbox/springboot/controller/HelloController.java
+++ springboot-example/src/main/java/com/scheakur/sandbox/springboot/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.scheakur.sandbox.springboot.controllers;
+package com.scheakur.sandbox.springboot.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
