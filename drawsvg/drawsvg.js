f395830be77732f899c923395f6b4258d7488c8b
scheakur <scheakur@gmail.com>
2014-10-05 12:28:43 +0900

    Draw lines

--- drawsvg/drawsvg.js
+++ drawsvg/drawsvg.js
@@ -14,6 +14,19 @@
   }
 
 
+  function drawLine(x1, y1, x2, y2) {
+    var path = paths.Path()
+      .moveto(x1, y1)
+      .lineto(x2, y2);
+
+    svg().appendChild(newElem('path', {
+      d: path.print(),
+      stroke: 'black',
+      'stroke-width': 5
+    }));
+  }
+
+
   function drawBox(x, y, w, h) {
     var path = paths.Path()
       .moveto(x, y)
@@ -30,29 +43,49 @@
     }));
   }
 
-
-  function pos(event) {
+  function pos(event, basePos) {
     return {
-      x: event.pageX,
-      y: event.pageY
+      x: event.pageX - basePos.x,
+      y: event.pageY - basePos.y
     };
+  }
+
+  var tools = {
+    line: function(start, end, width, height) {
+      drawLine(start.x, start.y, end.x, end.y);
+    },
+    box: function(start, end, width, height) {
+      if (Math.abs(width) > 10 && Math.abs(height) > 10) {
+        drawBox(start.x, start.y, width, height);
+      }
+    }
+  };
+
+  function getCurrentTool() {
+    var tool = document.querySelector('input[name="tool"]:checked').value;
+    return tools[tool] || function () {};
   }
 
 
   function main() {
+    var canvas = document.querySelector('.canvas').getBoundingClientRect();
+
+    var basePos = {
+      x: canvas.x,
+      y: canvas.y
+    };
+
     var start = null;
 
     document.addEventListener('mousedown', function(event) {
-      start = pos(event);
+      start = pos(event, basePos);
     }, false);
 
     document.addEventListener('mouseup', function(event) {
-      var end = pos(event);
+      var end = pos(event, basePos);
       var width = end.x - start.x;
       var height = end.y - start.y;
-      if (Math.abs(width) > 10 && Math.abs(height) > 10) {
-        drawBox(start.x, start.y, width, height);
-      }
+      getCurrentTool()(start, end, width, height);
     }, false);
   }
 
