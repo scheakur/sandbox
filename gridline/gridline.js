432e674760cf149f57a93b62676619c7d76c875e
scheakur <scheakur@gmail.com>
2015-02-18 09:04:24 +0900

    Draw sub grid

--- gridline/gridline.js
+++ gridline/gridline.js
@@ -15,25 +15,41 @@
       return;
     }
 
-    var image = createCrossImage(size);
+    var image = createCrossImage(size, 10);
 
     document.body.style.backgroundRepeat = 'repeat';
     document.body.style.backgroundImage = 'url(' + image + ')';
   }
 
-  function createCrossImage(size) {
+  function createCrossImage(size, subLines) {
     var canvas = document.createElement('canvas');
     canvas.width = size;
     canvas.height = size;
     var half = (size % 2 === 0) ? (size + 1) / 2 : size / 2;
     var ctx = canvas.getContext('2d');
+
+    var subSize = size / subLines;
+    for (var i = 1; i <= subLines / 2; i++) {
+      [1, -1].forEach(function(sign) {
+        var diff = sign * i * subSize;
+        ctx.beginPath();
+        ctx.moveTo(0, half + diff);
+        ctx.lineTo(size, half + diff);
+        ctx.moveTo(half + diff, 0);
+        ctx.lineTo(half + diff, size);
+        ctx.strokeStyle = '#eee';
+        ctx.stroke();
+      });
+    }
+
     ctx.beginPath();
     ctx.moveTo(0, half);
     ctx.lineTo(size, half);
     ctx.moveTo(half, 0);
     ctx.lineTo(half, size);
-    ctx.strokeStyle = '#ddd';
+    ctx.strokeStyle = '#76d3e6';
     ctx.stroke();
+
     return canvas.toDataURL();
   }
 
