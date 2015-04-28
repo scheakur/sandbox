dc1f9e860d6386cfe9da2b4cac6cc12687ca0c9a
scheakur <scheakur@gmail.com>
2015-04-28 12:23:56 +0900

    Add example to do something periodically

---
+++ groovy-snippets/DoPeriodically.groovy
@@ -0,0 +1,27 @@
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+
+def doPeriodically(fn, initDelay, period) {
+	def executor = Executors.newSingleThreadScheduledExecutor()
+	def i = 0
+
+	executor.scheduleWithFixedDelay({ // or scheduleAtFixedRate
+		try {
+			def doNext = fn(i++, executor)
+			if (!doNext) {
+				executor.shutdown()
+			}
+		} catch (e) {
+			try {
+				e.printStackTrace()
+			} finally {
+				executor.shutdown()
+			}
+		}
+	}, initDelay, period, TimeUnit.SECONDS)
+}
+
+doPeriodically({ i, executor ->
+	println('foo' + i)
+	return i < 10
+}, 0, 1)
