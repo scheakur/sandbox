7b35cf27b7afc8ff05d121fa168d8f72c8ee1579
scheakur <scheakur@gmail.com>
2015-09-24 23:39:20 +0900

    Add react-motion-hello

---
+++ react-motion-hello/app.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {Spring} from 'react-motion';
+
+export default class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      open: false,
+    };
+  }
+
+  handleMouseDown() {
+    this.setState({
+      open: !this.state.open,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onMouseDown={this.handleMouseDown.bind(this)}>Toggle</button>
+        <Spring defaultValue={{val: 0}} endValue={{val: this.state.open ? 400 : 0}}>
+          {interpolated =>
+            <div className="demo0-block" style={{
+              width: '100px',
+              height: '100px',
+              backgroundColor: 'red',
+              transform: `translate3d(${interpolated.val}px, 0, 0)`,
+            }} />
+          }
+        </Spring>
+      </div>
+    );
+  }
+
+}
---
+++ react-motion-hello/main.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import App from './app.jsx';
+
+window.addEventListener('DOMContentLoaded', () => {
+  React.render(<App/>, document.body);
+});
