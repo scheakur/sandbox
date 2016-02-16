ac8229d7a9d82f624f63c21c55fbc8bb0eb97315
scheakur <scheakur@gmail.com>
2016-02-16 23:52:13 +0900

    Refactoring

--- gridline/gridline.js
+++ gridline/gridline.js
@@ -1,47 +1,4 @@
 (function(window, document) {
-
-  function main() {
-    var sizer = document.querySelector('.gridsize');
-    var min = sizer.min;
-    var max = sizer.max;
-    sizer.addEventListener('change', function(e) {
-      resetGrid(Number(e.target.value), min, max);
-    });
-    resetGrid(Number(sizer.value), min, max, document.body);
-  }
-
-  function resetGrid(size, min, max, container) {
-    if (isNaN(size) || size < min || size > max) {
-      return;
-    }
-
-    var image = createCrossImage(size, 10);
-
-    container.style.backgroundRepeat = 'repeat';
-    container.style.backgroundImage = 'url(' + image + ')';
-  }
-
-  function createCrossImage(size, subLines) {
-    var canvas = document.createElement('canvas');
-    canvas.width = size;
-    canvas.height = size;
-    var half = (size % 2 === 0) ? (size + 1) / 2 : size / 2;
-    var ctx = canvas.getContext('2d');
-
-    var subSize = size / subLines;
-    for (var i = 1; i <= subLines / 2; i++) {
-      [1, -1].forEach(function(sign) {
-        var diff = sign * i * subSize;
-        drawLine(ctx, half + diff, half + diff, size, '#eee');
-      });
-    }
-
-    drawLine(ctx, half, half, size, '#76d3e6');
-
-    return canvas.toDataURL();
-  }
-
-
   function drawLine(context, cx, cy, size, color) {
     context.beginPath();
     context.moveTo(0, cy);
@@ -52,6 +9,54 @@
     context.stroke();
   }
 
-  window.addEventListener('DOMContentLoaded', main, false);
 
+  function createCrossImage(size, subLines) {
+    const canvas = document.createElement('canvas');
+    canvas.width = size;
+    canvas.height = size;
+    const half = (size % 2 === 0) ? (size + 1) / 2 : size / 2;
+    const ctx = canvas.getContext('2d');
+    const subSize = size / subLines;
+
+    function drawLineAt(diff) {
+      drawLine(ctx, half + diff, half + diff, size, '#eee');
+    }
+
+    for (let num = 1; num <= subLines / 2; num++) {
+      const diff = num * subSize;
+      drawLineAt(diff);
+      drawLineAt(-1 * diff);
+    }
+
+    drawLine(ctx, half, half, size, '#76d3e6');
+
+    return canvas.toDataURL();
+  }
+
+
+  function resetGrid(size, min, max, container) {
+    if (isNaN(size) || size < min || size > max) {
+      return;
+    }
+
+    const image = createCrossImage(size, 10);
+
+    container.style.backgroundRepeat = 'repeat';
+    container.style.backgroundImage = 'url(' + image + ')';
+  }
+
+
+  function main() {
+    const sizer = document.querySelector('.gridsize');
+    const min = sizer.min;
+    const max = sizer.max;
+    const container = document.body;
+    sizer.addEventListener('change', function(event) {
+      resetGrid(Number(event.target.value), min, max, container);
+    });
+    resetGrid(Number(sizer.value), min, max, container);
+  }
+
+
+  window.addEventListener('DOMContentLoaded', main, false);
 })(window, document);
