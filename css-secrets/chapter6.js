205449affd40848dbd0bb5cd7f5e0555c9d05f8d
scheakur <scheakur@gmail.com>
2016-10-27 22:48:36 +0900

    Fix section number

--- css-secrets/chapter6.js
+++ css-secrets/chapter6.js
@@ -1,35 +1,35 @@
 window.addEventListener('DOMContentLoaded', function() {
 
-  document.querySelector('.css30-1 button').addEventListener('click', function() {
-    var img = document.querySelector('.css30-1 img');
-    img.classList.toggle('css30-1-1');
+  document.querySelector('.css32-1 button').addEventListener('click', function() {
+    var img = document.querySelector('.css32-1 img');
+    img.classList.toggle('css32-1-1');
   });
 
-  document.querySelector('.css30-1 img').addEventListener('click', function() {
-    var img = document.querySelector('.css30-1 img');
-    img.classList.add('css30-1-1');
+  document.querySelector('.css32-1 img').addEventListener('click', function() {
+    var img = document.querySelector('.css32-1 img');
+    img.classList.add('css32-1-1');
   });
 
-  document.querySelector('.css30-2 button').addEventListener('click', function() {
-    var dialog = document.querySelector('.css30-2 dialog');
+  document.querySelector('.css32-2 button').addEventListener('click', function() {
+    var dialog = document.querySelector('.css32-2 dialog');
     dialog.showModal();
   });
 
-  document.querySelector('.css30-2 img').addEventListener('click', function() {
-    var dialog = document.querySelector('.css30-2 dialog');
+  document.querySelector('.css32-2 img').addEventListener('click', function() {
+    var dialog = document.querySelector('.css32-2 dialog');
     dialog.close();
   });
 
-  document.querySelector('.css31 button').addEventListener('click', function() {
-    var dialog = document.querySelector('.css31 dialog');
+  document.querySelector('.css33 button').addEventListener('click', function() {
+    var dialog = document.querySelector('.css33 dialog');
     dialog.showModal();
-    document.body.classList.add('css31-1');
+    document.body.classList.add('css33-1');
   });
 
-  document.querySelector('.css31 img').addEventListener('click', function() {
-    var dialog = document.querySelector('.css31 dialog');
+  document.querySelector('.css33 img').addEventListener('click', function() {
+    var dialog = document.querySelector('.css33 dialog');
     dialog.close();
-    document.body.classList.remove('css31-1');
+    document.body.classList.remove('css33-1');
   });
 
 });
