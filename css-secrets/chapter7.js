da2c085fefc986ca3c0a5f05c870c0112f95a21a
scheakur <scheakur@gmail.com>
2016-11-02 22:03:12 +0900

    Add sample CSS

---
+++ css-secrets/chapter7.js
@@ -0,0 +1,8 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+  document.querySelector('.css41 button').addEventListener('click', function() {
+    var optionalText = document.querySelector('.css41 div');
+    optionalText.classList.toggle('css41-1');
+  });
+
+});
